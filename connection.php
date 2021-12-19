b757aae8bb1a3528fbf7f695ec791401064c754c
yusptar <yusupvatar@gmail.com>
2021-12-19 16:47:05 +0700

    update docker

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 
-    $server = "localhost";
+    $server = "172.20.0.2";
     $user = "root";
-    $password = "";
+    $password = "admin";
     $db_name = "arsipdb";
 
     $db = mysqli_connect($server, $user, $password, $db_name);
